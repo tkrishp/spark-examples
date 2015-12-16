87352e1dc3f22d7ae81ac722ef53b578daadaadf
Tulasi Paradarami <tparadarami@23andme.com>
2015-12-16 12:44:19 -0800

    wordcount example

--- src/main/java/com/tulc/spark/examples/SparkDriver.java
+++ src/main/java/com/tulc/spark/examples/SparkDriver.java
@@ -1,12 +1,17 @@
 package com.tulc.spark.examples;
 
+import java.util.Arrays;
+import java.util.Map;
+
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
 
 public class SparkDriver {
-
+    public static JavaSparkContext sc = null;
     public static void main(String[] args) {
-        JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("SparkExamples").setMaster("local[*]"));
+        sc = new JavaSparkContext(new SparkConf().setAppName("SparkExamples").setMaster("local[*]"));
         if (args.length == 0) {
             System.out.println("Missing spark application name.");
             System.out.println("Examples: [WordCount, Variance, Mean]");
@@ -14,11 +19,21 @@ public class SparkDriver {
         }
         else {
             if (args[0].equals("WordCount")) {
-                WordCount wc = new WordCount(sc);
+                wordCount();
             }
         }
-        
-
     }
-
+    
+    public static void wordCount() {
+        JavaRDD<String> lines = sc.textFile("hdfs://trinity/data/input.txt");
+        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+                    public Iterable<String> call(String line) {
+                        return Arrays.asList(line.split(" "));
+                    } 
+                });
+        Map<String, Long> hm = words.countByValue();
+        for (String word : hm.keySet()) 
+            System.out.println(word + "," + hm.get(word));
+        System.out.println("Total words: " + words.count());
+    }
 }
